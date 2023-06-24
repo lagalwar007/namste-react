7a296dc0d0d5163aed4d43146b37bf489cd3848d
karan-lagalwar <lagalwar007@gmail.com>
2023-06-25 00:41:11 +0530

    save changes: for setting up VS code and using React.createElementAPI

--- Chapter 01- Inception/App.js
+++ Chapter 01- Inception/App.js
@@ -1,6 +1,59 @@
-const heading = React.createElement('h1', {
-    id: 'heading',
-}, "Hello world from React!!!");
+/**
+ *
+ *  <div id="parent">
+ *      <div id="child1">
+ *          <div id="list">
+ *              <ul>
+ *                  <li>A</li>
+ *                  <li>B</li>
+ *                  <li>C</li>
+ *                  <li>D</li>
+ *              </ul>
+ *          </div>
+ *      </div>
+ *      <div id="child2">
+ *          <div id="list">
+ *              <ul>
+ *                  <li>A</li>
+ *                  <li>B</li>
+ *                  <li>C</li>
+ *                  <li>D</li>
+ *              </ul>
+ *          </div>
+ *      </div>
+ *  </div>
+ *
+ *
+ **/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const heading = React.createElement('div', { id: 'parent' }, [
+  React.createElement(
+    'div',
+    { id: 'child1', key: '1'},
+    React.createElement(
+      'div',
+      { id: 'list' },
+      React.createElement('ul', {}, [
+        React.createElement('li', {key: '1'}, 'A'),
+        React.createElement('li', {key: '2'}, 'B'),
+        React.createElement('li', {key: '3'}, 'C'),
+      ])
+    )
+  ),
+  React.createElement(
+    'div',
+    { id: 'child2', key: '2' },
+    React.createElement(
+      'div',
+      { id: 'list' },
+      React.createElement('ul', {}, [
+        React.createElement('li', {key: '1'}, 'A'),
+        React.createElement('li', {key: '2'}, 'B'),
+        React.createElement('li', {key: '3'}, 'C'),
+      ])
+    )
+  ),
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
